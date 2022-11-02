9d50abf7d5cb18ff49752094a8561951229fd1c5
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-02 19:48:46 +0530

    selected

--- blood.py
+++ blood.py
@@ -24,14 +24,18 @@ while True:
         print("view student")
     if(choice==2):
         print("view blooddonor")
-        print("view employee")
-        sql='SELECT * FROM `blood`' 
+        sql='SELECT * FROM `bloodbank`' 
         mycursor.execute(sql)
         result=mycursor.fetchall()
         for i in result:
             print(i)
     elif(choice==3):
         print('search blooddonor')
+        phoneno=input("enter phoneno:")
+        sql='SELECT `id`, `name`, `address`, `phoneno`, `bloodgroup` FROM `bloodbank` WHERE `phoneno`='+phoneno
+        mycursor.execute(sql)
+        result=mycursor.fetchall()
+        print(result)
     elif(choice==4):
         print('update the blooddonor')
     elif(choice==5):
